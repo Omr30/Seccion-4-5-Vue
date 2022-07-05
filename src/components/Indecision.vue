145fa08231d0dda658899cf371d4dcd593793f3b
mvrk <oscarmenacho23@gmail.com>
2022-07-05 15:38:35 -0500

    Watch - Observar cambios en una propiedad reactiva

--- src/components/Indecision.vue
+++ src/components/Indecision.vue
@@ -1,14 +1,85 @@
 <template>
-  <h1>Indecision</h1>
+  <img src="https://via.placeholder.com/250" alt="bg">
+  <div class="bg-dark"></div>
+
+    <div class="indecision-container">
+
+        <input 
+            v-model="question"
+            type="text" 
+            placeholder="Hazme una pregunta"
+        >
+        <p>Recuerda terminar con un signo de interrogación (?)</p>
+
+        <div>
+            <h2>{{question}}</h2>
+            <h1>Si, No, ...pensando</h1>
+        </div>
+
+    </div>
+
 </template>
 
 <script>
 export default {
     name: 'Indecision',
-    
+    data(){
+        return {
+            question: null
+        }
+    },
+    watch: {
+        question( value, oldValue ){
+            
+             if( !value.includes('?') ) return
+
+        }
+    }
+
 }
 </script>
 
-<style>
+<style scoped>
+    img, .bg-dark {
+        height: 100vh;
+        left: 0px;
+        max-height: 100%;
+        max-width: 100%;
+        position: fixed;
+        top: 0px;
+        width: 100vw;
+    }
 
+    .bg-dark {
+        background-color: rgba(0, 0, 0, 0.4);
+    }
+
+    .indecision-container {
+        position: relative;
+        z-index: 99;
+    }
+    
+    input {
+        width: 250px;
+        padding: 10px 15px;
+        border-radius: 5px;
+        border: none;
+    }
+    input:focus {
+        outline: none;
+    }
+
+    p {
+        color: white;
+        font-size: 20px;
+        margin-top: 0px;
+    }
+
+    h1, h2 {
+        color: white;
+    }
+    
+    h2 { 
+        margin-top: 150px;
+    }
 </style>
